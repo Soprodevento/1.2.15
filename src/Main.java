7ac6ab267994aa118a2455909665f22e4ed8cffe
Soprodevento <Sashinka2803@gmail.com>
2024-02-27 01:00:17 +0300

    Исправил

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,7 @@ public class Main {
     public static void main(String[] args) {
         maxLongSqr();
     }
-
     public static void maxLongSqr() {
-        BigInteger maxLongValue = BigInteger.valueOf(Long.MAX_VALUE);
-        BigInteger square = maxLongValue.multiply(maxLongValue);
-        System.out.println("Квадрат максимального значения для long: " + square);
+      System.out.println( BigInteger.valueOf(Long.MAX_VALUE).multiply(BigInteger.valueOf(Long.MAX_VALUE)));
     }
 }
